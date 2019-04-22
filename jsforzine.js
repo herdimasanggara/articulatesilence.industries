320802cc04399e04eb309439a6b0d9269434ca5a
herdimasanggara <47119995+herdimasanggara@users.noreply.github.com>
2019-04-22 10:43:21 -0400

    Add files via upload

--- jsforzine.js
+++ jsforzine.js
@@ -1,108 +1,108 @@
 $(document).ready(function() {
 
-  var padOne = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/SD0025.mp3');
+  var padOne = new Audio('Antye Greie.mp3');
   $('.pad-1').mousedown(function() {
     padOne.load()
     padOne.play();
   });
 
-  var padTwo = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/SD0010.mp3');
+  var padTwo = new Audio('Battles.mp3');
   $('.pad-2').mousedown(function() {
     padTwo.load();
     padTwo.play();
   });
 
-  var padThree = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/SD0000.mp3');
+  var padThree = new Audio('Diamanda Galas.mp3');
   $('.pad-3').mousedown(function() {
     padThree.load();
     padThree.play();
   });
 
-  var padFour = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/RS.mp3');
+  var padFour = new Audio('Eli Keszler.mp3');
   $('.pad-4').mousedown(function() {
     padFour.load();
     padFour.play();
   });
 
-  var padFive = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/OH25.mp3');
+  var padFive = new Audio('Eliane Radigue.mp3');
   $('.pad-5').mousedown(function() {
     padFive.load();
     padFive.play();
   });
 
-  var padSix = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/MA.mp3');
+  var padSix = new Audio('Grouper.mp3');
   $('.pad-6').mousedown(function() {
     padSix.load();
     padSix.play();
   });
 
-  var padSeven = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/CY0010.mp3');
+  var padSeven = new Audio('Holly Herndon.mp3');
   $('.pad-7').mousedown(function() {
     padSeven.load();
     padSeven.play();
   });
 
-  var padEight = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/CH.mp3');
+  var padEight = new Audio('Jacob Kirkegaard.mp3');
   $('.pad-8').mousedown(function() {
     padEight.load();
     padEight.play();
   });
 
-  var padNine = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/CB.mp3');
+  var padNine = new Audio('Jlin.mp3');
   $('.pad-9').mousedown(function() {
     padNine.load();
     padNine.play();
   });
 
-  var padTen = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/BD0010.mp3');
+  var padTen = new Audio('Okkyung Lee.mp3');
   $('.pad-10').mousedown(function() {
     padTen.load();
     padTen.play();
   });
 
-  var padEleven = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/BD0000.mp3');
+  var padEleven = new Audio('Oneohtrix Point Never.mp3');
   $('.pad-11').mousedown(function() {
     padEleven.load();
     padEleven.play();
   });
 
-  var padTwelve = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/CP.mp3');
+  var padTwelve = new Audio('Oren Ambarchi.mp3');
   $('.pad-12').mousedown(function() {
     padTwelve.load();
     padTwelve.play();
   });
 
-  var padThirteen = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/BD0000.mp3');
+  var padThirteen = new Audio('Pauline Oliveros.mp3');
   $('.pad-13').mousedown(function() {
     padThirteen.load();
     padThirteen.play();
   });
 
-  var padFourteen = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/BD0010.mp3');
+  var padFourteen = new Audio('Ryoji Ikeda.mp3');
   $('.pad-14').mousedown(function() {
     padFourteen.load();
     padFourteen.play();
   });
 
-  var padFifteen = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/CB.mp3');
+  var padFifteen = new Audio('Sachiko M.mp3');
   $('.pad-15').mousedown(function() {
     padFifteen.load();
     padFifteen.play();
   });
 
-  var padSixteen = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/CH.mp3');
+  var padSixteen = new Audio('Senyawa.mp3');
   $('.pad-16').mousedown(function() {
     padSixteen.load();
     padSixteen.play();
   });
 
-  var padSeventeen = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/CY0010.mp3');
+  var padSeventeen = new Audio('Tim Hecker.mp3');
   $('.pad-17').mousedown(function() {
     padSeventeen.load();
     padSeventeen.play();
   });
 
-  var padEighteen = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/MA.mp3');
+  var padEighteen = new Audio('Uwalmassa.mp3');
   $('.pad-18').mousedown(function() {
     padEighteen.load();
     padEighteen.play();
