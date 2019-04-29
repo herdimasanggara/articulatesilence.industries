479554b19bb0c261964ae194ee0fa5c86bd4ccc3
herdimasanggara <47119995+herdimasanggara@users.noreply.github.com>
2019-04-29 12:48:58 -0400

    Add files via upload

--- jsforzine.js
+++ jsforzine.js
@@ -198,3 +198,23 @@ $(document).ready(function() {
   });
 
 });
+
+var $win = $(window),
+    w = 0,h = 0,
+    rgb = [],
+    getWidth = function() {
+        w = $win.width();
+        h = $win.height();
+    };
+
+$win.resize(getWidth).mousemove(function(e) {
+
+    rgb = [
+        Math.round(e.pageX/w * 255),
+        Math.round(e.pageX/w * 255),
+        Math.round(e.pageX/w * 255)
+    ];
+
+    $(document.body).css('background','rgb('+rgb.join(',')+')');
+
+}).resize();
